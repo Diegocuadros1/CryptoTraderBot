973b78959e9286dadb845b363b117dd94e53ff5c
Diego Cuadros <cuadrosda21@gmail.com>
2024-12-20 10:14:19 -0800

    starting on monitoring wallets

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
-from solana.rpc.api import Client
-from solders.pubkey import Pubkey
-from solana.rpc.websocket_api import connect
-from solana.rpc.async_api import AsyncClient
+import time
 import datetime
 import asyncio
+import os
+import dotenv
+import asyncio
+import websockets
+import json as JSON
+
+dotenv.load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+ws_url = f"wss://mainnet.helius-rpc.com/?api-key={API_KEY}"
+rate_limit = asyncio.Semaphore(10) # Allow only 10 concurrent subscriptions per minute
+
+# http_client = Client("https://api.mainnet-beta.solana.com")
+    
+async def subscribe(id, pubkey, websocket):
+    async with rate_limit:  
+        subscription_payload = {
+            "jsonrpc": "2.0",
+            "id": id,
+            "method": "accountSubscribe",
+            "params": [
+                pubkey,
+                {
+                    "encoding": "jsonParsed"
+                }
+            ]
+        }
+        await websocket.send(JSON.stringify(subscription_payload))
+
+        await asyncio.sleep(6)  # 60 seconds / 10 requests
 
 
-http_client = Client("https://api.mainnet-beta.solana.com")
+async def subscribe_to_wallets(wallets, websocket):
+    tasks = []
+    for idx, wallet in enumerate(wallets):
+        tasks.append(subscribe(idx, wallet, websocket))
+    await asyncio.gather(*tasks)
+
 
 async def monitor_wallets(wallets):
-    async with connect("wss://api.mainnet-beta.solana.com") as websocket:
-        subscriptions = {}
+    async with websockets.connect(ws_url) as websocket:
+        print("Connected to WebSocket.")
 
-        for wallet in wallets:
-            try:
-                await websocket.account_subscribe(wallet, encoding="jsonParsed") 
-            except Exception as e:
-                print(f"Error subscribing to account: {wallet}. Error: {e}")
-                continue
+        await subscribe_to_wallets(wallets, websocket) # Subscribe to wallets with a certain rate limit
 
         while True:
             try:
                 message = await websocket.recv()
-                print("message: " + str(message))                
+                print("message: " + str(message))
+                print("Time: " + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
             except Exception as e:
                 print(f"Time: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}. Error: {e}")
@@ -35,10 +63,11 @@ async def main():
     with open('./data/wallets.txt', 'r') as f:
         for line in f:
             try:
-                wallets.append(Pubkey.from_string(line.strip()))
+                print(line.strip())
+                wallets.append(line.strip())
             except:
-                print("incorrect wallet accessed: " + line)
-        
+                print("error: " + line)
+    
     await monitor_wallets(wallets)
 
 
